f6836b9b883f3d4862afe1aa82792d6b98f40eb5
Adavi <sadavi@icc11400.iccohio.com>
2016-07-19 15:16:58 -0400

    trying to get endpoints working

--- src/main/java/com/icct/hibernatehelloworld/controller/MessageController.java
+++ src/main/java/com/icct/hibernatehelloworld/controller/MessageController.java
@@ -4,16 +4,16 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
 import com.icct.hibernateworld.service.MessageService;
 
-@Controller
+@RestController
 @RequestMapping("/message")
 public class MessageController {
 	
@@ -34,7 +34,7 @@ public class MessageController {
 	}
 	
 	@RequestMapping(value = "/read", method = RequestMethod.GET)
-	public @ResponseBody List readMessage(){
+	public  List readMessage(){
 		System.out.println("here");
 		List messages = null;
 		
